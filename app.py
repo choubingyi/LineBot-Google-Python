9075c46994872d5d5660fc71e28cd1d3bb5fdde8
jack.chou <jack.chou@quanta.corp-partner.google.com>
2018-11-19 18:00:46 +0800

    update calendar query

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+#!/usr/bin/env python
 # encoding: utf-8
+
 from flask import Flask, request, abort
 
 from linebot import (
@@ -13,19 +15,18 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-# 填入你的 message api 資訊
-line_bot_api = LineBotApi('')
-handler = WebhookHandler('')
+line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
+handler = WebhookHandler('YOUR_CHANNEL_SECRET')
 
-# 設定你接收訊息的網址，如 https://YOURAPP.herokuapp.com/callback
-@app.route("/", methods=['POST'])
-def index():
+
+@app.route("/callback", methods=['POST'])
+def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
     # get request body as text
     body = request.get_data(as_text=True)
-    print("Request body: " + body, "Signature: " + signature)
+    app.logger.info("Request body: " + body)
 
     # handle webhook body
     try:
@@ -38,12 +39,10 @@ def index():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    print("Handle: reply_token: " + event.reply_token + ", message: " + event.message.text)
-    content = "{}: {}".format(event.source.user_id, event.message.text)
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=content))
+        TextSendMessage(text=event.message.text))
 
-import os
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=os.environ['PORT'])
+    app.run()
